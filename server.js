c9fde92d7192a2a46ec03f489913ee685fc027db
Sourav Malo <sourav.cse5.bu.s1@gmail.com>
2022-07-05 08:51:18 +0600

    delete articles implemented

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Article = require('./models/articleSchema')
 const articleRouter = require('./router/articleRouter')
 const app = express()
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find()
